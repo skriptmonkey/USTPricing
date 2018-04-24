16b133509770d03bb71a3c34038faaeb84155aca
Alvin Morris <skriptmonkey@gmail.com>
2018-04-24 21:21:28 +0000

    Used base_dir instead of static path

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/home/alvin/code/py/USTPricing/config/secret_key.txt') as f:
+with open(os.path.join(BASE_DIR, 'config/secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
