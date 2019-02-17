3854439f4f0111e82b7b0c863d713d4a21ffd0ff
Alvin Morris <skriptmonkey@gmail.com>
2019-02-16 19:53:54 -0900

    Decided to leave in the capitals in the url path alongside the lowercase.

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('ustpricing/', include('USTPricing.urls')),
+    path('USTPricing/', include('USTPricing.urls')),
     path('admin/', admin.site.urls),
 ]
